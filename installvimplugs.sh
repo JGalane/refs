efffd19af953ee9cb613f75ac168c99ce2fae2d4
James Galane <JamesGalane@yahoo.com>
2022-03-04 20:21:44 -0700

    added vim plugin install script

---
+++ installvimplugs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Currently using pathogen cause... I'm too lazy to investigate anything else lol
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle
+
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
+git clone https://github.com/preservim/tagbar.git
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/uiiaoo/java-syntax.vim.git
+git clone https://github.com/bfrg/vim-cpp-modern.git
+git clone https://github.com/aklt/plantuml-syntax.git
+git clone https://github.com/vim-scripts/CycleColor.git
+git clone https://github.com/rafi/awesome-vim-colorschemes.git
+
