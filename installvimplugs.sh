07d016117665efdc1ad3c9865ec180abdf5025f7
JamesGalane <JamesGalane@yahoo.com>
2022-03-12 10:04:47 -0700

    removed color cycle

--- installvimplugs.sh
+++ installvimplugs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+CWD=$(pwd)
+
 # Currently using pathogen cause... I'm too lazy to investigate anything else lol
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle
+rm -rf *
 
 git clone https://github.com/ctrlpvim/ctrlp.vim.git
 git clone https://github.com/preservim/tagbar.git
@@ -13,6 +16,6 @@ git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/uiiaoo/java-syntax.vim.git
 git clone https://github.com/bfrg/vim-cpp-modern.git
 git clone https://github.com/aklt/plantuml-syntax.git
-git clone https://github.com/vim-scripts/CycleColor.git
 git clone https://github.com/rafi/awesome-vim-colorschemes.git
 
+cd $CWD
